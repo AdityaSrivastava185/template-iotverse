3eaf01f3fbb8cab231a6a5dc731a9dd369b54e75
Aditya-Srivastava <pks85715@gmail.com>
2025-05-27 23:06:28 +0530

    responsive feature added for product list

--- src/app/components/(Navbar)/Navbar.tsx
+++ src/app/components/(Navbar)/Navbar.tsx
@@ -1,6 +1,9 @@
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
+    const router = useRouter();
     return (
         <div>
             <main className="relative z-10">
@@ -44,7 +47,7 @@ const Navbar = () => {
                         </a>
                     </div>
 
-                    <a href="/products" className="hidden md:flex bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md items-center transition cursor-pointer">
+                    <Link href="/products" className="hidden md:flex bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md items-center transition cursor-pointer">
                         Explore Products
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -60,9 +63,9 @@ const Navbar = () => {
                                 d="M14 5l7 7m0 0l-7 7m7-7H3"
                             />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <button type="button" className="md:hidden text-white bg-gradient-to-r from-indigo-600 via-indigo-600 to-indigo-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2 ">Log In</button>
+                    <button type="button" onClick={() => router.push('/products')} className="md:hidden text-white bg-gradient-to-r from-indigo-600 via-indigo-600 to-indigo-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2 ">Explore Products</button>
 
                 </nav>
             </main>
